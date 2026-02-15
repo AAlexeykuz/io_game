c6c6b6bde8d5adc22eac33a734e3cbafdb841aa0
AAlexeykuz <alexeykuzmichenko@gmail.com>
2026-02-15 23:31:13 +0300

    init

---
+++ server/server.py
@@ -0,0 +1,28 @@
+import asyncio
+
+import websockets
+
+# удалить потом из глобального контекста
+connected_players = set()
+
+
+async def handle_connection(websocket) -> None:
+    print(type(websocket))
+    connected_players.add(websocket)
+    try:
+        async for message in websocket:
+            await asyncio.gather(
+                *[user.send(message) for user in connected_players]
+            )
+    finally:
+        connected_players.remove(websocket)
+
+
+async def main() -> None:
+    async with websockets.serve(handle_connection, "localhost", 8080):
+        print("Сервер запущен")
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
