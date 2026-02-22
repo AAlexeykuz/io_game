9ef65446de8228f408df5c4725792f36d2c1ce79
Alexandr-ru <alexlagutkin07@yandex,ru>
2026-02-22 16:15:16 +0300

    remove unused argument in move()

--- game/game.py
+++ game/game.py
@@ -1,16 +1,17 @@
-DTIME = 1/20
+DTIME = 1 / 20
+
 
 class Player:
     speed: float = 5
 
     def __init__(self, x: float, y: float):
-        self.x: float = x #начальная координата x
-        self.y: float = y #начальная координата y
-        self.vx: float = 0.0 #направление движения по оси X
-        self.vy: float = 0.0 #направление движения по оси Y
-    
+        self.x: float = x  # начальная координата x
+        self.y: float = y  # начальная координата y
+        self.vx: float = 0.0  # направление движения по оси X
+        self.vy: float = 0.0  # направление движения по оси Y
+
     def normalize_velocity(self) -> None:
-        length = (self.vx**2 + self.vy**2)**0.5
+        length = (self.vx**2 + self.vy**2) ** 0.5
         self.vx /= length
         self.vy /= length
 
@@ -19,6 +20,6 @@ class Player:
         self.vy = vy
         self.normalize_velocity()
 
-    def move(self, x:float, y: float) -> None:
+    def move(self) -> None:
         self.x += self.vx * self.speed * DTIME
         self.y += self.vy * self.speed * DTIME
